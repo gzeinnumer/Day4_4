bac12fdd91b548e56c6deb8126b71e9cfa67caa9
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-04-07 13:25:44 +0700

    commit

--- app/src/main/java/com/gzeinnumer/day4_4/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/day4_4/MainActivity.java
@@ -1,10 +1,17 @@
 package com.gzeinnumer.day4_4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<User> list = new ArrayList<>();
     private RecyclerView rvData ;
     private RvAdapter adapter;
+    private EditText edSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +31,59 @@ public class MainActivity extends AppCompatActivity {
 
         //todo 6
         rvData = findViewById(R.id.rv);
+        edSearch = findViewById(R.id.ed_search);
 
         list = User.generateDataItem();
+        list.add(new User("Zein", "Zein aja"));
         adapter = new RvAdapter(getApplicationContext(), list);
         rvData.setAdapter(adapter);
         rvData.hasFixedSize();
         rvData.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        edSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                adapter.getFilter().filter(editable.toString());
+            }
+        });
+    }
+
+    //todo 8
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+
+        filterFunction(menu);
+
+        return true;
+    }
+
+    private void filterFunction(Menu menu) {
+        MenuItem searchItem = menu.findItem(R.id.showSearchTitle);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/gzeinnumer/day4_4/RvAdapter.java
+++ app/src/main/java/com/gzeinnumer/day4_4/RvAdapter.java
@@ -4,21 +4,34 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 //todo 4
-public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyHolder> {
+public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyHolder> implements Filterable {
     private Context context;
     private List<User> list;
+    private List<User> listFilter;
 
     public RvAdapter(Context context, List<User> list) {
         this.context = context;
         this.list = list;
+        this.listFilter = new ArrayList<>(list);
+    }
+
+    public void setList(ArrayList<User> list) {
+        this.list = list;
+        this.listFilter = new ArrayList<>(list);
+        notifyDataSetChanged();
     }
 
     @NonNull
@@ -47,4 +60,43 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyHolder> {
             description =  itemView.findViewById(R.id.description);
         }
     }
+
+    @Override
+    public Filter getFilter() {
+        return exampleFilter;
+    }
+
+    private Filter exampleFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<User> fildteredList = new ArrayList<>();
+            if(constraint == null || constraint.length() == 0){
+                Collections.sort(listFilter, new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        return o1.getUsername().toLowerCase().compareTo(o2.getUsername().toLowerCase());
+                    }
+                });
+                fildteredList.addAll(listFilter);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for (User item : listFilter){
+                    if( item.getUsername().toLowerCase().contains(filterPattern)){
+                        fildteredList.add(item);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = fildteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            list.clear();
+            list.addAll((List)results.values);
+            notifyDataSetChanged();
+        }
+    };
 }
